27ed22be4dc87524f2b3460a1a538434c7678cdc
Bentley Paton <bentley-paton@hotmail.com>
2020-07-21 15:55:01 +0100

    added tests and amended add_item function to increase quantity

--- shopping_basket/shopping_basket_tests/test_shopping_basket.py
+++ shopping_basket/shopping_basket_tests/test_shopping_basket.py
@@ -14,3 +14,25 @@ def test_add_item_to_basket():
 
     assert len(basket.contents) == 1
     assert basket.contents['peach'] == 2
+
+def test_add_multiple_items_to_basket():
+
+    basket = Basket()
+
+    basket.add_item('peach', 1)
+    basket.add_item('tomato', 3)
+    basket.add_item('corn', 8)
+
+    assert len(basket.contents) == 3
+    assert basket.contents['tomato'] == 3
+    assert basket.contents['peach'] == 1
+    assert basket.contents['corn'] == 8
+
+def test_add_same_item_increases_quantity():
+
+    basket = Basket()
+
+    basket.add_item('peach', 1)
+    basket.add_item('peach', 3)
+
+    assert basket.contents['peach'] == 4
